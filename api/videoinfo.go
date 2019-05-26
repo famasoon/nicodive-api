8f318f3c7931cc1b00259fd9bb27f95f0a6a325f
famasoon <famasoon@gmail.com>
2019-05-26 22:13:35 +0900

    Add json annotation

--- api/videoinfo.go
+++ api/videoinfo.go
@@ -11,56 +11,56 @@ import (
 const VIDEO_INFO_ENDPOINT = "http://api.ce.nicovideo.jp/nicoapi/v1/video.info?v="
 
 type NicovideoVideoResponse struct {
-	XMLName xml.Name `xml:"nicovideo_video_response"`
-	Text    string   `xml:",chardata"`
-	Status  string   `xml:"status,attr"`
+	XMLName xml.Name `xml:"nicovideo_video_response" json:"xml_name"`
+	Text    string   `xml:",chardata" json:"text"`
+	Status  string   `xml:"status,attr" json:"status"`
 	Video   struct {
-		Text                      string `xml:",chardata"`
-		ID                        string `xml:"id"`
-		UserID                    string `xml:"user_id"`
-		Deleted                   string `xml:"deleted"`
-		Title                     string `xml:"title"`
-		Description               string `xml:"description"`
-		LengthInSeconds           string `xml:"length_in_seconds"`
-		ThumbnailURL              string `xml:"thumbnail_url"`
-		UploadTime                string `xml:"upload_time"`
-		FirstRetrieve             string `xml:"first_retrieve"`
-		DefaultThread             string `xml:"default_thread"`
-		ViewCounter               string `xml:"view_counter"`
-		MylistCounter             string `xml:"mylist_counter"`
-		OptionFlagCommunity       string `xml:"option_flag_community"`
-		OptionFlagNicowari        string `xml:"option_flag_nicowari"`
-		OptionFlagMiddleThumbnail string `xml:"option_flag_middle_thumbnail"`
-		OptionFlagDmcPlay         string `xml:"option_flag_dmc_play"`
-		CommunityID               string `xml:"community_id"`
-		VitaPlayable              string `xml:"vita_playable"`
-		PpvVideo                  string `xml:"ppv_video"`
-		Permission                string `xml:"permission"`
-		ProviderType              string `xml:"provider_type"`
+		Text                      string `xml:",chardata" json:"text"`
+		ID                        string `xml:"id" json:"id"`
+		UserID                    string `xml:"user_id" json:"user_id"`
+		Deleted                   string `xml:"deleted" json:"deleted"`
+		Title                     string `xml:"title" json:"title"`
+		Description               string `xml:"description" json:"description"`
+		LengthInSeconds           string `xml:"length_in_seconds" json:"length_in_seconds"`
+		ThumbnailURL              string `xml:"thumbnail_url" json:"thumbnail_url"`
+		UploadTime                string `xml:"upload_time" json:"upload_time"`
+		FirstRetrieve             string `xml:"first_retrieve" json:"first_retrieve"`
+		DefaultThread             string `xml:"default_thread" json:"default_thread"`
+		ViewCounter               string `xml:"view_counter" json:"view_counter"`
+		MylistCounter             string `xml:"mylist_counter" json:"mylist_counter"`
+		OptionFlagCommunity       string `xml:"option_flag_community" json:"option_flag_community"`
+		OptionFlagNicowari        string `xml:"option_flag_nicowari" json:"option_flag_nicowari"`
+		OptionFlagMiddleThumbnail string `xml:"option_flag_middle_thumbnail" json:"option_flag_middle_thumbnail"`
+		OptionFlagDmcPlay         string `xml:"option_flag_dmc_play" json:"option_flag_dmc_play"`
+		CommunityID               string `xml:"community_id" json:"community_id"`
+		VitaPlayable              string `xml:"vita_playable" json:"vita_playable"`
+		PpvVideo                  string `xml:"ppv_video" json:"ppv_video"`
+		Permission                string `xml:"permission" json:"permission"`
+		ProviderType              string `xml:"provider_type" json:"provider_type"`
 		Options                   struct {
-			Text           string `xml:",chardata"`
-			Mobile         string `xml:"mobile,attr"`
-			Sun            string `xml:"sun,attr"`
-			LargeThumbnail string `xml:"large_thumbnail,attr"`
-			Adult          string `xml:"adult,attr"`
-		} `xml:"options"`
-	} `xml:"video"`
+			Text           string `xml:",chardata" json:"text"`
+			Mobile         string `xml:"mobile,attr" json:"mobile"`
+			Sun            string `xml:"sun,attr" json:"sum"`
+			LargeThumbnail string `xml:"large_thumbnail,attr" json:"large_thumbnail"`
+			Adult          string `xml:"adult,attr" json:"adult"`
+		} `xml:"options" json:"options"`
+	} `xml:"video" json:"video"`
 	Thread struct {
-		Text        string `xml:",chardata"`
-		ID          string `xml:"id"`
-		NumRes      string `xml:"num_res"`
-		Summary     string `xml:"summary"`
-		CommunityID string `xml:"community_id"`
-		GroupType   string `xml:"group_type"`
-	} `xml:"thread"`
+		Text        string `xml:",chardata" json:"text"`
+		ID          string `xml:"id" json:"id"`
+		NumRes      string `xml:"num_res" json:"numres"`
+		Summary     string `xml:"summary" json:"summry"`
+		CommunityID string `xml:"community_id" json:"community_id"`
+		GroupType   string `xml:"group_type" json:"grouptype"`
+	} `xml:"thread" json:"thread"`
 	Tags struct {
-		Text    string `xml:",chardata"`
+		Text    string `xml:",chardata" json:"text"`
 		TagInfo []struct {
-			Text string `xml:",chardata"`
-			Tag  string `xml:"tag"`
-			Area string `xml:"area"`
-		} `xml:"tag_info"`
-	} `xml:"tags"`
+			Text string `xml:",chardata" json:"text"`
+			Tag  string `xml:"tag" json:"tag"`
+			Area string `xml:"area" json:"area"`
+		} `xml:"tag_info" json:"tag_info"`
+	} `xml:"tags" json:"tags"`
 }
 
 func GetVideoInfo(videoID string) (*NicovideoVideoResponse, error) {
